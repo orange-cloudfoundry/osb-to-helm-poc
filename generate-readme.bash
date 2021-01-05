37bfaa6ce466f756139ecba4e51f170b832c42c4
Guillaume Berche <guillaume.berche@orange.com>
2021-01-05 17:48:24 +0100

    Refactor README.md generation

--- generate-readme.bash
+++ generate-readme.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # ./gomplate --help
-./gomplate -d servicedefinition=./sample-p-mysql-catalog.json --file ./readme.tpl | tee mysql-charts/p-mysql/README.md
+./gomplate -d servicedefinition=./sample-p-mysql-catalog.json --file ./readme.md.tpl | tee mysql-charts/p-mysql/README.md
